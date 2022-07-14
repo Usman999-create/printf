d60c61bd7aae863db1997ed59b4d7c81cc603a9b
usman yusuf <yusuf4all99@gmail.com>
2022-07-14 21:10:21 +0000

    printf_rot13.c

--- printf_rot13.c
+++ printf_rot13.c
@@ -1,39 +1,37 @@
-#include "main.h" 
- #include <stdlib.h> 
-  
- /** 
-  * print_R - prints a string in rot13 
-  * @R: string to print 
-  * 
-  * Return: number of chars printed 
-  */ 
- int print_R(va_list R) 
- { 
-         char *str; 
-         unsigned int i, j; 
-         int count = 0; 
-         char in[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"; 
-         char out[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"; 
-  
-         str = va_arg(R, char *); 
-         if (str == NULL) 
-                 str = "(ahyy)"; 
-         for (i = 0; str[i]; i++) 
-         { 
-                 for (j = 0; in[j]; j++) 
-                 { 
-                         if (in[j] == str[i]) 
-                         { 
-                                 _putchar(out[j]); 
-                                 count++; 
-                                 break; 
-                         } 
-                 } 
-                 if (!in[j]) 
-                 { 
-                         _putchar(str[i]); 
-                         count++; 
-                 } 
-         } 
-         return (count); 
- }
+#include "main.h"
+/**
+ * printf_rot13 - printf str to ROT13 place into buffer
+ * @args: type struct va_arg where is allocated printf arguments
+ *
+ * Return: counter
+ */
+int printf_rot13(va_list args)
+{
+	int i, j, counter = 0;
+	int k = 0;
+	char *s = va_arg(args, char*);
+	char alpha[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+	char beta[] = {"nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"};
+
+	if (s == NULL)
+		s = "(null)";
+	for (i = 0; s[i]; i++)
+	{
+		k = 0;
+		for (j = 0; alpha[j] && !k; j++)
+		{
+			if (s[i] == alpha[j])
+			{
+				_putchar(beta[j]);
+				counter++;
+				k = 1;
+			}
+		}
+		if (!k)
+		{
+			_putchar(s[i]);
+			counter++;
+		}
+	}
+	return (counter);
+}
